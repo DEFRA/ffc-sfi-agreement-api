4b9f42dab5da6a310a9c555f6952351a7b405dde
John Watson <johnwatson484@hotmail.com>
2021-12-12 16:27:36 +0000

    Add removal

--- app/routes/agreement-submit.js
+++ app/routes/agreement-submit.js
@@ -18,6 +18,11 @@ module.exports = [{
     handler: async (request, h) => {
       const agreement = await getAgreement(request.payload.agreementNumber, request.payload.sbi)
       if (agreement?.agreementData?.agreement) {
+        for (const funding in agreement.agreementData.agreement.action) {
+          if (!agreement.agreementData.agreement.action[funding].active) {
+            delete agreement.agreementData.agreement.action[funding]
+          }
+        }
         await sendAgreementSubmitMessage(agreement.agreementData.agreement)
         return h.response(`Agreement Submitted: ${agreement.agreementNumber}`)
           .code(201)
